c3a007d59187e8ef9e512f645e05f8bf1bab361a
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-08-05 18:34:36 +0200

    Update TestiBars.js

--- TestiBars.js
+++ TestiBars.js
@@ -45,7 +45,10 @@
                              div.append($('<a href="' + social.flickr + '">' + '<img src="http://static.viewbook.com/images/social_icons/flickr_16.png" style="padding:0px;" />' + '</a>' ));
                              div.append(' ')
                           };
-                          
+                          if (social.flickrGal) {
+                             div.append($('<a href="' + social.flickrGal + '">' + 'Gal' + '</a>'));
+                             div.append(' ')
+                           };
                            
                           return div
                    };         
