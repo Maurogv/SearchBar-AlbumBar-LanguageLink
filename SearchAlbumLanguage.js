ab2e7a94de807ea4fdf84d0672429860038ff514
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-08-12 21:37:56 +0200

    Update SearchAlbumLanguage.js

--- SearchAlbumLanguage.js
+++ SearchAlbumLanguage.js
@@ -8,57 +8,57 @@
 
 
        var searchBar= function (social) {
-                      this.color=color;
-                      this.id = 'searchBar';                   
-
-                      var div= bar();
-                      div.attr('id', this.id);
-                      if (!social) { 
-                             div.append($('<a href="" style="color:' + this.color + ';">&bnsp;</a>'));
-                             return div;
-                      }
-                      if (social.google) {
-                         div.append($('<a href="https://www.google.it/webhp?rls=ig&amp;gws_rd=ssl#q=' + window.keyword + '">' + '<img src="http://icons.iconarchive.com/icons/danleech/simple/16/google-icon.png" />' + '</a>'));
-                         div.append(' ')
-                       };
-                       if (social.facebook) {
-                         div.append($('<a href="https://www.facebook.com/search/str/' + window.keyword +'/keywords_top">' + '<img src="http://static.viewbook.com/images/social_icons/facebook_16.png" />' + '</a>' ));
-                         div.append(' ')
-                       };
-                       if (social.instagram) {
-                         div.append($('<a href="https://instagram.com/explore/tags/' + window.keyword + '/">' + '<img src="http://icons.iconarchive.com/icons/designbolts/free-instagram/16/Active-Instagram-2-icon.png" />' + '</a>'));
-                         div.append(' ')
-                       };
-                       if (social.flickr) {
-                         div.append($('<a href="https://www.flickr.com/search/?text=' + window.keyword +'">' + '<img src="http://static.viewbook.com/images/social_icons/flickr_16.png" />' + '</a>' ));
-                         div.append(' ')
-                       };
-                       return div
+                             this.color=color;
+                             this.id = 'searchBar';                   
+       
+                             var div= bar();
+                             div.attr('id', this.id);
+                             if (!social) { 
+                                    div.append($('<a href="" style="color:' + this.color + ';">&bnsp;</a>'));
+                                    return div;
+                             }
+                             if (social.google) {
+                                div.append($('<a href="https://www.google.it/webhp?rls=ig&amp;gws_rd=ssl#q=' + window.keyword + '">' + '<img src="http://icons.iconarchive.com/icons/danleech/simple/16/google-icon.png" />' + '</a>'));
+                                div.append(' ')
+                              };
+                              if (social.facebook) {
+                                div.append($('<a href="https://www.facebook.com/search/str/' + window.keyword +'/keywords_top">' + '<img src="http://static.viewbook.com/images/social_icons/facebook_16.png" />' + '</a>' ));
+                                div.append(' ')
+                              };
+                              if (social.instagram) {
+                                div.append($('<a href="https://instagram.com/explore/tags/' + window.keyword + '/">' + '<img src="http://icons.iconarchive.com/icons/designbolts/free-instagram/16/Active-Instagram-2-icon.png" />' + '</a>'));
+                                div.append(' ')
+                              };
+                              if (social.flickr) {
+                                div.append($('<a href="https://www.flickr.com/search/?text=' + window.keyword +'">' + '<img src="http://static.viewbook.com/images/social_icons/flickr_16.png" />' + '</a>' ));
+                                div.append(' ')
+                              };
+                              return div
                 };            
                       
       
          var albumBar = function (social) {
-                                    this.color=color;
-                                    var div= bar();
+                             this.color=color;
+                             var div= bar();
 
-                                    // to override .post-body in blogspot
-                                    div.attr('font-size', '100%');
-                                    div.attr('line-height', '1.1');                                                
-                                    if (social.google) {
-                                       div.append($('<a href="' + social.google + '">' + '<img src="http://ssl.gstatic.com/images/icons/gplus-16.png" style="padding:0px;" />' + '</a>'));
-                                       div.append(' ')
-                                    };
-                                    if (social.flickr) {
-                                       div.append($('<a href="' + social.flickr + '">' + '<img src="http://static.viewbook.com/images/social_icons/flickr_16.png" style="padding:0px;" />' + '</a>' ));
-                                       div.append(' ')
-                                    };
-                                    if (social.flickrGal) {
-                                       div.append($('<a href="' + social.flickrGal + '">' + 'Gal' + '</a>'));
-                                       div.append(' ')
-                                     };
-                                     
-                                    return div
-                            };
+                             // to override .post-body in blogspot
+                             div.attr('font-size', '100%');
+                             div.attr('line-height', '1.1');                                                
+                             if (social.google) {
+                                div.append($('<a href="' + social.google + '">' + '<img src="http://ssl.gstatic.com/images/icons/gplus-16.png" style="padding:0px;" />' + '</a>'));
+                                div.append(' ')
+                             };
+                             if (social.flickr) {
+                                div.append($('<a href="' + social.flickr + '">' + '<img src="http://static.viewbook.com/images/social_icons/flickr_16.png" style="padding:0px;" />' + '</a>' ));
+                                div.append(' ')
+                             };
+                             if (social.flickrGal) {
+                                div.append($('<a href="' + social.flickrGal + '">' + 'Gal' + '</a>'));
+                                div.append(' ')
+                              };
+                              
+                             return div
+              };
                   
 
         var languageLink=  function (blog, lang, post) { 
