4f251db52bccc868b2073ebce41b135ddcddf01c
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-08-13 21:50:19 +0200

    Update SearchAlbumLanguage.js

--- SearchAlbumLanguage.js
+++ SearchAlbumLanguage.js
@@ -18,7 +18,7 @@
                                     return div;
                              }
                              if (social.google) {
-                                div.append($('<a href="https://www.google.it/webhp?rls=ig&amp;gws_rd=ssl#q=' + this.keyword.google ? window.keyword : this.keyword.google + '">' + '<img src="http://icons.iconarchive.com/icons/danleech/simple/16/google-icon.png" />' + '</a>'));
+                                div.append($('<a href="https://www.google.it/webhp?rls=ig&amp;gws_rd=ssl#q=' + window.keyword + '">' + '<img src="http://icons.iconarchive.com/icons/danleech/simple/16/google-icon.png" />' + '</a>'));
                                 div.append(' ')
                               };
                               if (social.facebook) {
