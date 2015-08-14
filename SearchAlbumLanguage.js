daeb7ea74c807a4ed61fa66a84bcde60a772eb7c
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-08-14 20:40:01 +0200

    Update SearchAlbumLanguage.js

--- SearchAlbumLanguage.js
+++ SearchAlbumLanguage.js
@@ -24,7 +24,7 @@
                               };
                               if (social.facebook) {
                                 socialKeyword= keyword.facebook===undefined ? window.keyword : keyword.facebook;   
-                                div.append($('<a href="https://www.facebook.com/search/str/' + socialKeyword +'/keywords_top">' + '<img src="http://static.viewbook.com/images/social_icons/facebook_16.png" />' + '</a>' ));
+                                div.append($('<a href="https://www.facebook.com/search/str/' + social.facebook +'/keywords_top">' + '<img src="http://static.viewbook.com/images/social_icons/facebook_16.png" />' + '</a>' ));
                                 div.append(' ')
                               };
                               if (social.instagram_tags) {
