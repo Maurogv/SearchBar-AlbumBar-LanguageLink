c1cbc1c7e8ed5d2fcc9d64f975b70690980044fa
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-09-21 13:13:13 +0200

    Update SearchAlbumLanguage.js

--- SearchAlbumLanguage.js
+++ SearchAlbumLanguage.js
@@ -83,31 +83,32 @@
                                  flickr_gallery='https://www.flickr.com/photos/95893200@N07/galleries/',
                                  google='https://photos.google.com/u/0/album/',
                                  onedrive='https://onedrive.live.com/?id=B65050E4B8CE1B76!',
-                                 onedrive_end='&cid=B65050E4B8CE1B76&group=0';
+                                 onedrive_end='&cid=B65050E4B8CE1B76&group=0',
+                                 padding0px='padding:0px;';
                                  
                              if (social.google) {
-                                div.append($('<a href="' + google + social.google + '">' + '<img src="' + icons.googlePlus + '" style="padding:0px;" />' + '</a>'));
+                                div.append($('<a href="' + google + social.google + '">' + '<img src="' + icons.googlePlus + '" style="' + padding0px + '" />' + '</a>'));
                                 div.append(' ')
                              };
                              if (social.flickr_sets) {
-                                div.append($('<a href="' + flickr_set + social.flickr_sets + '">' + '<img src="' + icons.flickr + '" style="padding:0px;" />' + '</a>' ));
+                                div.append($('<a href="' + flickr_set + social.flickr_sets + '">' + '<img src="' + icons.flickr + '" style="' + padding0px + '" />' + '</a>' ));
                                 div.append(' ')
                              };
                              if (social.flickr_galleries) {
-                                div.append($('<a href="' + flickr_gallery + social.flickr_galleries + '">' + '<img src="' + icons.flickr + '" style="padding:0px;" />' + '</a>'));
+                                div.append($('<a href="' + flickr_gallery + social.flickr_galleries + '">' + '<img src="' + icons.flickr + '" style="' + padding0px + '" />' + '</a>'));
                                 div.append(' ')
                               };
                               if (social.facebook) {
-                                div.append($('<a href="' + facebook + social.facebook + facebook_end + '">' + '<img src="' + icons.facebook + '" style="padding:0px;" />' + '</a>'));
+                                div.append($('<a href="' + facebook + social.facebook + facebook_end + '">' + '<img src="' + icons.facebook + '" style="' + padding0px + '" />' + '</a>'));
                                 div.append(' ')
                               };
                               if (social.onedrive) {
-                                div.append($('<a href="' + onedrive + social.onedrive + onedrive_end + '">' + '<img src="' + icons.onedrive + '" style="padding:0px;" />' + '</a>'));
+                                div.append($('<a href="' + onedrive + social.onedrive + onedrive_end + '">' + '<img src="' + icons.onedrive + '" style="' + padding0px + '" />' + '</a>'));
                                 div.append(' ')
                               };
                               if (social.blogger) {
                                 var domain= social.blogger.replace(/-\s/g,'').replace(/\s/g,'-').replace("'",'').toLowerCase();
-                                div.append($('<a href="' + domain + '.html">' + '<img src="' + icons.blogger + '" style="padding:0px;" />' + '</a>'));
+                                div.append($('<a href="' + domain + '.html">' + '<img src="' + icons.blogger + '" style="' + padding0px + ' float:right;" />' + '</a>'));
                                 div.append(' ')
                               };
                              
