5d0d14a45db0bf899d728863bd6838939a2d84b3
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-03 11:46:32 +0100

    Update addContent.js

--- js/addContent.js
+++ js/addContent.js
@@ -6,6 +6,6 @@
          scriptTag=document.getElementsByTagName(script)[0];
          scriptTag.parentNode.insertBefore(newScript,scriptTag);
          alert(url);
-      })
+      });
     	alert (window.writing);
    })(window,document,'script',['https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/ContentsBars_LanguageLink.js'])
