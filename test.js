127353b4d821443a822ce11115392c3645159a0a
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-03 09:34:47 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 (function (window) {
-   alert( window['wr'].q.slice.call(arguments));
+   var a=window['wr'].q.slice.call(arguments);
+   for(i=0; i&lt; a.length; i++){
+        alert("parameter " + i + " = " + a[i]); 
+    }
+   alert( );
    alert('hello');
 })(window);
