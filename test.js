ed566c580f1be584117d09974f6bbd8fe50afe55
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-03 09:36:14 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 (function (window) {
    var a=window['wr'].q.slice.call(arguments);
-   for(i=0; i&lt; a.length; i++){
+   for(i=0; i< a.length; i++){
         alert("parameter " + i + " = " + a[i]); 
     }
    alert( );
