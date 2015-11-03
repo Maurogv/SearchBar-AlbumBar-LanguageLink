0ea6cd34a3b9e2e02875355cda59a089859325c1
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-03 13:16:51 +0100

    Update album.js

--- json/album.js
+++ json/album.js
@@ -1,3 +1,13 @@
 {
-   
+   'grignone' : {
+       'acquafredda' : { 'photos_google': 'AF1QipOhBtls_rJhKIuUzIjdGYegeWLv6-H72wotvfPl', 
+                         'plus_google': 6035146133779205969,
+                         'flickr_sets': 72157645232281769
+        }
+       'agriturismo Brunino' : { 'photos_google': 'AF1QipObkSWY2-tMGvcjdM71DmoAr3LhfymZLtwbCLNs', 
+                                 'plus_google': 6164040280865064465, 
+                                 'flickr_sets': 72157652653491493, 
+                                 'flickr_galleries: 72157656348361735
+       }
+   }
 }
