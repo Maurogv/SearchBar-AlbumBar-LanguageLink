375e48c1efdc0430866fc85136d9df6e49b9219c
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-03 11:39:01 +0100

    Create addContent.js

---
+++ js/addContent.js
@@ -0,0 +1,11 @@
+(function(window,document,script, urls){
+      urls.forEach(function(url) {
+         newScript=document.createElement(script);
+         newScript.async=1;
+         newScript.src=url;
+         scriptTag=document.getElementsByTagName(script)[0];
+         scriptTag.parentNode.insertBefore(newScript,scriptTag);
+         alert(url);
+      })
+    	alert (window.writing);
+   })(window,document,'script',['https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/ContentsBars_LanguageLink.js'])
