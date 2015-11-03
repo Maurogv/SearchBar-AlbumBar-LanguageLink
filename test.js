59adb53d6ba52620a5beb7cc4c2006edb6e08c1b
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-03 10:07:32 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,3 @@
 (function (window) {
-   var a=window['wr'].q.slice.call(arguments);
-   for(i=0; i< a.length; i++){
-        alert("parameter " + i + " = " + a[i]); 
-    }
-   alert( window.par);
+   alert( window.site);
 })(window);
