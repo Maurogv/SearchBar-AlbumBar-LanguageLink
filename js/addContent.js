593d4a521485f776c253514f3e83154fd88992bb
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-04 21:46:38 +0100

    Update addContent.js

--- js/addContent.js
+++ js/addContent.js
@@ -20,7 +20,8 @@
       // Start polling...
      checkReady(function ($) {
         // Use $ here...
-       	albumsUrl();
+        alert('ciao');
+        albumsUrl();
      }) 
      
    })(window,document,'script',['https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', 'https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/albumsUrl.js'])
