39aba52e40388001d7b78608dddcdb60c1550474
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-04 11:42:02 +0100

    Update albumsHref.js

--- js/albumsHref.js
+++ js/albumsHref.js
@@ -1,14 +1,24 @@
-function (album) {
-
- $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/album.json")
-                  .done(function (album) {                   
-                      alert (JSON.stringify(album['grignone'].acquafredda));         
+function albumsHref() {
+  var albumsCodes, albumsFixedPart;
+  $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/album.json")
+                  .done(function (albums) {                   
+                      albumsCodes=albums;
+                      alert (JSON.stringify(albums['grignone'].acquafredda));         
                   })
                   .fail(function( jqxhr, textStatus, error ) {
                         var err = textStatus + ", " + error;
                         alert( "Request Failed: " + err );
                   });
-    
+                  
+  $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/album.json")
+                  .done(function (albums) {                   
+                      albumsFixedPart=albums;
+                  })
+                  .fail(function( jqxhr, textStatus, error ) {
+                        var err = textStatus + ", " + error;
+                        alert( "Request Failed: " + err );
+                  });                 
+                  
 }
     
     
