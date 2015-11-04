541e152acec7da9319008cb0a7d4722633966014
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-04 10:44:21 +0100

    Update toHrefAlbum.js

--- js/toHrefAlbum.js
+++ js/toHrefAlbum.js
@@ -1,1 +1,14 @@
+function (album) {
 
+  var facebook_set='https://www.facebook.com/mauro.vezzoli.77/media_set?set=a.',
+      facebook_end='.100000433081467&type=3',
+      flickr_sets='https://www.flickr.com/photos/95893200@N07/sets/',
+      flickr_galleries='https://www.flickr.com/photos/95893200@N07/galleries/',
+      photos_google='https://photos.google.com/u/0/album/',
+      plus_google='https://plus.google.com/u/0/photos/113727750810625155496/albums/'
+      onedrive='https://onedrive.live.com/?id=B65050E4B8CE1B76!',
+      onedrive_end='&cid=B65050E4B8CE1B76&group=0';
+    
+}
+    
+    
