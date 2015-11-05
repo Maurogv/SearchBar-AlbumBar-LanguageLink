2291703226193b50a985b67f7ade527fcd56f0a7
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-05 09:14:17 +0100

    Update albumsUrl.js

--- js/albumsUrl.js
+++ js/albumsUrl.js
@@ -6,7 +6,7 @@ window.albumsUrl= function (writing) {
 
   $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsCodes.json")
                   .done(function (albumsCodes) {                   
-                      
+                      alert (JSON.stringify(albumsCodes[topic][title])); 
                   })
                   .fail(function( jqxhr, textStatus, error ) {
                         var err = textStatus + ", " + error;
@@ -22,7 +22,7 @@ window.albumsUrl= function (writing) {
                         alert( "Request Failed: " + err );
                   });   
                   
-  alert (JSON.stringify(albumsCodes[topic][title]));             
+              
                   
 }
     
