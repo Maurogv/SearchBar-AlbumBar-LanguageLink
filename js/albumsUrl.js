101f898206995a43bfcc8437ae8a2acaf5831c91
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-06 13:04:14 +0100

    Update albumsUrl.js

--- js/albumsUrl.js
+++ js/albumsUrl.js
@@ -1,19 +1,25 @@
 window.albumsUrl= function (writing) {
   
-  var topic=writing[1],
-      title=writing[2];
-  
-  var albumsCodes, albumsFixedPart;
-  
+  var topic=window.writing[1],
+      title=window.writing[2];
+
   var firstPromise = $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsCodes.json");
   var secondPromise = $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsFixedPart.json");
 
-  $.when(firstPromise, secondPromise).done(function(firstData, secondData) {
-    // do something
-     alert(firstData['grignone']);
+  $.when(firstPromise, secondPromise).done(function(albumsCodes, albumsFixedPart) {
+     // [0] value, [1] success
+     currentAlbumCodes=albumsCodes[0][topic][title];
+     albumsUrl=[];
+     Object.keys(currentAlbumCodes).forEach(function (key) {
+        // do something with obj[key]
+        prefixAlbum=albumsFixedPart[0][key];  
+        url=prefixAlbum+ currentAlbumCodes[key];
+        url+= albumsFixedPart[0][key+'_end'] ? albumsFixedPart[0][key+'_end'] : '';
+        albumsUrl.push({key,url});
+     });
   });
-  
-  return null;
+  alert(albumsUrl);
+  return albumsUrl;
 
 /*  $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsCodes.json")
                   .done(function (data) {
