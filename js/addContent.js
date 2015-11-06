933b395889bdf0790bf473aeab1378b8457a8eb5
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-06 18:31:12 +0100

    Update addContent.js

--- js/addContent.js
+++ js/addContent.js
@@ -22,6 +22,7 @@
         // Use $ here...
         albumsUrl(window.writing);
         var bar=new window.bar();
+        bar.append($('<a href="" style="color:' + '#E0E0E0' + ';">&bnsp;</a>'));
         bar.insertBefore(document.getElementsByTagName(script)[0].parentNode);
      }) 
      
