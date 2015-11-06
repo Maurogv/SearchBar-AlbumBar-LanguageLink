2f0bf4f7102b8bdba606b88df4cb05cd6e61798b
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-06 13:33:56 +0100

    Update albumsUrl.js

--- js/albumsUrl.js
+++ js/albumsUrl.js
@@ -22,26 +22,6 @@ window.albumsUrl= function (writing) {
   });
   
   return null;
-/*  $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsCodes.json")
-                  .done(function (data) {
-                      albumsCodes(data);
-                  })
-                  .fail(function( jqxhr, textStatus, error ) {
-                        var err = textStatus + ", " + error;
-                        alert( "Request Failed: " + err );
-                  });
-                  
-  $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsFixedPart.json")
-                  .done(function (data) {                   
-                      albumsFixedPart=data;
-                  })
-                  .fail(function( jqxhr, textStatus, error ) {
-                        var err = textStatus + ", " + error;
-                        alert( "Request Failed: " + err );
-                  });   
-                  
- var albumsCodes=function albumCodes(data) {return data;}
- alert(albumsCodes); */
                   
 }
     
