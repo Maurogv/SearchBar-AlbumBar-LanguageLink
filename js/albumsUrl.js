7390dad4c05cf549c6efbe8b96bdef21392e5e1d
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-09 09:12:04 +0100

    Update albumsUrl.js

--- js/albumsUrl.js
+++ js/albumsUrl.js
@@ -1,26 +1,26 @@
-window.albumsUrl= function (writing) {
-  
-  var topic=window.writing[1],
-      title=window.writing[2];
+window.albumsUrl= function (writing, callback) {
+      
+     var topic=window.writing[1],
+         title=window.writing[2],
+         urls={};
 
-  var firstPromise = $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsCodes.json");
-  var secondPromise = $.getJSON("https://raw.githubusercontent.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsFixedPart.json");
-
-  $.when(firstPromise, secondPromise).done(function(albumsCodes, albumsFixedPart) {
-     // [0] value, [1] success
-     currentAlbumCodes=albumsCodes[0][topic][title];
-     urls=[];
-     Object.keys(currentAlbumCodes).forEach(function (key) {
-        // do something with obj[key]
-        prefixAlbum=albumsFixedPart[0][key];  
-        url=prefixAlbum+ currentAlbumCodes[key];
-        url+= albumsFixedPart[0][key+'_end'] ? albumsFixedPart[0][key+'_end'] : '';
-        urls.push({key:key, url:url});
-     });
-     return urls;
-  });
-  
-  return null;
+      var firstPromise = $.getJSON("https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsCodes.json");
+      var secondPromise = $.getJSON("https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/albumsFixedPart.json");
+     
+      $.when(firstPromise, secondPromise).done(function(albumsCodes, albumsFixedPart) {         
+         // [0] value, [1] success 
+         currentAlbumCodes=albumsCodes[0][topic][title];        
+         
+         Object.keys(currentAlbumCodes).forEach(function (key) {
+            // do something with obj[key]
+            prefixAlbum=albumsFixedPart[0][key];  
+            url=prefixAlbum+ currentAlbumCodes[key];
+            url+= albumsFixedPart[0][key+'_end'] ? albumsFixedPart[0][key+'_end'] : '';
+            urls[key]=url;
+           // urls.push({key:key, url:url});
+         }); 
+        callback(urls);   
+       });     
                   
 }
     
