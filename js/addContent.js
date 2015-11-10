8af132507e369eafbc56f7ead67c8a218cd31adb
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-10 11:31:06 +0100

    Update addContent.js

--- js/addContent.js
+++ js/addContent.js
@@ -1,29 +1,30 @@
 (function(window,document,script, urls){
-      urls.forEach(function(url) {
-         newScript=document.createElement(script);
-         newScript.async=1;
-         newScript.src=url;
-         scriptTag=document.getElementsByTagName(script)[0];
-         scriptTag.parentNode.insertBefore(newScript,scriptTag);
-      })
+  urls.forEach(function(url) {
+     newScript = document.createElement(script);
+     newScript.async =1 ;
+     newScript.src = url;
+     scriptTag = document.getElementsByTagName(script)[0];
+     scriptTag.parentNode.insertBefore(newScript,scriptTag);
+  })
     	
-    	// Poll for jQuery to come into existance
-       var checkReady = function (callback) {
-        if (window.jQuery) {
-             callback(jQuery);
-        }
-        else {
-             window.setTimeout(function () { checkReady(callback); }, 100);
-        }
-      };
+	// Poll for jQuery to come into existance
+  var checkReady = function (callback) {
+    if (window.jQuery) {
+      callback(jQuery);
+    }
+    else {
+      window.setTimeout(function () { checkReady(callback); }, 100);
+    }
+  };
 
-      // Start polling...
-     checkReady(function ($) {
-        // Use $ here...
-        window.createBar('search', window.writing, window.callbackCreate);
-        window.createBar('albums', window.writing, window.callbackCreate);
-     }) 
+  // Start polling...
+  checkReady(function ($) {
+    // Use $ here...
+    window.createBar('search', window.writing, window.callbackCreate);
+    window.createBar('albums', window.writing, window.callbackCreate);
+  }) 
      
-   })(window,document,'script',['https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
+  
+})(window,document,'script',['https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
                                 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/bar.js',
                                 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/createBar.js'])
