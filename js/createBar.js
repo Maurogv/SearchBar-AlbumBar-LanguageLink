a1796465909755dd429217d2c8bc403726ffcb05
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-14 11:27:54 +0100

    Update createBar.js

--- js/createBar.js
+++ js/createBar.js
@@ -40,7 +40,7 @@ window.search = function (callback) {
       currentSearch = search[topic][title];                
       Object.keys(currentSearch).forEach(function (key) {
         // do something with obj[key]
-        if (currentSearch[key].includes('{self}')) {
+        if ( currentSearch[key].indexOf('{self}') !=-1 ) {
           currentTitle=title;
           if ( key == 'https://www.facebook.com/{search}?fref=ts' |
                key == 'https://www.instagram.com/{search}' |
