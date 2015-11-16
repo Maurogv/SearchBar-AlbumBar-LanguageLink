4cba8b314d25eded384af8c2fc5bac857e1fb02d
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-16 21:53:12 +0100

    Update createBar.js

--- js/createBar.js
+++ js/createBar.js
@@ -21,7 +21,7 @@ window.albums = function (callback) {
           currentTitle = title.replace(/\w\S*/g, function(txt) {
              return txt.charAt(0).toUpperCase() + txt.substr(1);
           });
-          currentTitle = currentTitle.replace('(','').replace(')','').replace('-','');
+          currentTitle = currentTitle.replace('(','').replace(')','').replace('-','').replace(/'/g,'');
           valueAlbum = valueAlbum.replace('{self}', currentTitle.replace(/\s/g, ''));
         }
         url = key.replace('{album}',valueAlbum);
