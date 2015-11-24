597ffd0f8e39036a1153ecf41110c400923941e5
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-24 19:15:18 +0100

    Update addToBar.js

--- js/addToBar.js
+++ js/addToBar.js
@@ -105,18 +105,37 @@ window.addToBar = function (source, urls) {
 };
 
 window.redirect = function (callback) {
-  title = window.writing[0],
-  enTitle = window.writing[0],
-  topic = window.writing[1]; 
-
-  $.getJSON("https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/redirect.json").done(function(titles) {
-    Object.keys(titles[topic]).forEach(function (key) {
-      if ( titles[topic][key] == title ) {
+  title = window.writing[0],    
+  topic = window.writing[1],
+  lang = window.writing[2],
+  enTitle = window.writing[0];
+  
+  if ( lang == 'it') {
+    callback();
+    return;
+  };
+  
+  var topPromise = $.getJSON("https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/redirectTop.json");
+  var secondPromise = $.getJSON("https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/json/redirect.json");
+     
+  $.when(topPromise, secondPromise).done(function(topics, titles) {
+    currentTopics=topics[0];
+    Object.keys(currentTopics).forEach(function (key) {
+      if ( key == topic ) {
+        topic = currentTopics[key];
+        return true;
+      };
+    })  
+  
+    currentTitles=titles[0];
+    Object.keys(currentTitles[topic]).forEach(function (key) {
+      if ( currentTitles[topic][key] == title ) {
         enTitle = key;
         return true;
       };
     })  
-    title = titles[topic][title] || title;            
+    title = currentTitles[topic][title] || title;
+
     callback();
   })
 }
