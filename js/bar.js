f96eb58b47a39e25f9397f72273bbe365f0d811b
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-28 14:36:37 +0100

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -11,12 +11,13 @@
     if ( bar.length == 0 ) {
       bar = new window.bar();
       bar[0]['id'] = source + 'Bar';
-      body = document.getElementsByTagName('script')[0].parentNode;
       if (source == 'search') {
-       body.insertBefore(bar[0], body.firstChild);
+        body = document.getElementsByTagName('script')[0].parentNode;
+        body.insertBefore(bar[0], body.firstChild);
       }
       else if ( source == 'albums' ) { 
-        bar.appendTo(body);
+        window.addBottom();
+        div.prepend(bar);
       };
     };
    
