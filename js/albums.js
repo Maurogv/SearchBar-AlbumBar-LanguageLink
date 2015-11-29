17c8f95fc98c779ca38f653e6df63b5279f9dfde
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-29 17:34:49 +0100

    Update albums.js

--- js/albums.js
+++ js/albums.js
@@ -46,3 +46,14 @@ window.albums = function (callback) {
   }
 
 };
+
+String.prototype.deleteUnWantedChars = function() {
+  current = this.replace(/\w\S*/g, function(txt) {
+              return txt.charAt(0).toUpperCase() + txt.substr(1);
+            });
+  current = current.replace(/'\w/g, function(txt) {
+              return txt.charAt(0) + txt.charAt(1).toUpperCase() + txt.substr(2);
+            });
+  current = current.replace(/[()]/g, '').replace(/'/g, '');
+  return current.replace(/\s/g, '');
+}
