b52793d79725155f52fb52ea74a02a78ecf6cff7
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-29 19:52:07 +0100

    Update addContent.js

--- js/addContent.js
+++ js/addContent.js
@@ -22,11 +22,15 @@
     // Use $ here...
     window.search(window.icons);
     window.albums(window.icons);
-    window.posts(window.iconLanguagePost);
+    window.post(window.iconLanguagePost);
   }) 
      
-  
+ // 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/addToBar.js',
+ // 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/addPostToBar.js',
 })(window,document,'script',['https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/bar.js',
-                             'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/addToBar.js',
-                             'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/addPostToBar.js',
+                             'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/albums.js',
+                             'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/search.js',
+                             'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/post.js',
+                             'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/icons16x16.js',
+                             'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/redirect.js',
                              'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'])
