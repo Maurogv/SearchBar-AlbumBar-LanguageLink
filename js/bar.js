0c3628aa712496449963d1c74b4ce3620d73ac70
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2015-11-30 17:15:13 +0100

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -1,7 +1,7 @@
 (function (window) {
   var bar = function (color) {
     color = typeof color !== 'undefined' ? color : "#E0E0E0";
-    var div = $('<div style="background-color:' + color + '; padding-left : 5px;"></div>');
+    var div = $('<div style="background-color:' + color + ';"></div>');
     return div
   };
   window.bar = bar;
