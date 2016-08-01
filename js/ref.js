fa9db2287fccc57b6ef74c43bb83315d832d365f
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2016-08-01 21:13:59 +0200

    Update ref.js

--- js/ref.js
+++ js/ref.js
@@ -3,7 +3,7 @@
     var references = function () {          
         $("my-ref").each(function (index) {
             $(this).text("ciao");
-            $(this).append("<a href='" + url + "#" + $(this).attr("name") + "'> [" + (index + 1) + "]</a>");
+           // $(this).append("<a href='" + url + "#" + $(this).attr("name") + "'> [" + (index + 1) + "]</a>");
         });
     }
 
