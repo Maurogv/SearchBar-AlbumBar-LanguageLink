6ae2b661859e73e29ccc101d369bca336d1236bc
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2016-08-01 20:59:50 +0200

    Update ref.js

--- js/ref.js
+++ js/ref.js
@@ -1,13 +1,12 @@
 (function (window) {
-
-    var addReferences = function (url) {          
+    url='elenco';
+    var references = function (url) {          
         $("my-ref").each(function (index) {
-            $(this).attr("id", "cite_ref-" + (index + 1));
             $(this).text("");
-            $(this).append("<a href='" + url + $(this).attr("name") + "'> [" + (index + 1) + "]</a>");
+            $(this).append("<a href='" + url + "#" + $(this).attr("name") + "'> [" + (index + 1) + "]</a>");
         });
     }
 
-    window.I.addReferences = addReferences;
+    window.I.references = references;
 
 })(window);
