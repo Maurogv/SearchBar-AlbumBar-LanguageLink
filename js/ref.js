cf573f8c033f396164138938bb292a419d1404b0
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2016-08-01 21:12:20 +0200

    Update ref.js

--- js/ref.js
+++ js/ref.js
@@ -1,6 +1,6 @@
 (function (window) {
     url='elenco';
-    var references = function (url) {          
+    var references = function () {          
         $("my-ref").each(function (index) {
             $(this).text("ciao");
             $(this).append("<a href='" + url + "#" + $(this).attr("name") + "'> [" + (index + 1) + "]</a>");
