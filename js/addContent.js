0ebd778b37b81a274d3072955280444226f84af3
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2016-08-02 18:47:06 +0200

    Update addContent.js

--- js/addContent.js
+++ js/addContent.js
@@ -1,10 +1,19 @@
-(function(window,document,script, urls){
+(function(window, document, script, urls, link, csss){
   urls.forEach(function(url) {
      newScript = document.createElement(script);
      newScript.async = 1;
      newScript.src = url;
      scriptTag = document.getElementsByTagName(script)[0];
      scriptTag.parentNode.insertBefore(newScript, scriptTag);
+  })
+  
+  csss.forEach(function(css) {
+     newLink = document.createElement(link);
+     newLink.async = 1;
+     newLink.href = css;
+     newLink.type = 'text/css';
+     headTag = document.getElementsByTagName('head')[0];
+     newLink.appendTo(headTag);
   })
     	
   // Poll for jQuery to come into existance
@@ -36,6 +45,5 @@
                                 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/icons16x16.js',
                                 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/redirect.js',
                                 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/js/ref.js',
-                                { href : 'https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/css/ref.css',
-                                  type : "text/css" },
-                                'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'])
+                                'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'],
+                                'link', ['https://rawgit.com/Maurogv/SearchBar-MyContentBar-LanguageLink/master/css/ref.css'] )
