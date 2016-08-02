541060f35fdf439c171d4bd39ff057ca7ecfdf57
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2016-08-02 11:38:57 +0200

    Update ref.js

--- js/ref.js
+++ js/ref.js
@@ -1,9 +1,15 @@
 (function (window) {
-    url='elenco';
+
     var references = function () {          
         $("my-ref").each(function (index) {
-            $(this).text("ciao");
-           // $(this).append("<a href='" + url + "#" + $(this).attr("name") + "'> [" + (index + 1) + "]</a>");
+            $(this).attr("id", "cite_ref-" + (index + 1));
+            $(this).text("");
+            $(this).append("<a href=#cite_note-" + (index + 1) + "> [" + (index + 1) + "]</a>");
+        });
+        //  <li id="cite_note-1"><a href="#cite_ref-1"><b>^</b></a> Cite Note-1</li>
+        $(".references li").each(function (index) {
+            $(this).attr("id", "cite_note-" + (index + 1));
+            $(this).prepend("<a href=#cite_ref-" + (index + 1) + "><b>^</b></a> ");
         });
     }
 
