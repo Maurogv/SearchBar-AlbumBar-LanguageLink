60c5c9458f10cfcd87cf51bd4a983189d481abb1
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2016-08-02 11:57:58 +0200

    Update ref.js

--- js/ref.js
+++ js/ref.js
@@ -1,16 +1,12 @@
-(function (window) {
-
-    var references = function () {          
-        $("my-ref").each(function (index) {
-            $(this).attr("id", "cite_ref-" + (index + 1));
-            $(this).text("");
-            $(this).append("<a href=#cite_note-" + (index + 1) + "> [" + (index + 1) + "]</a>");
-        });
-        //  <li id="cite_note-1"><a href="#cite_ref-1"><b>^</b></a> Cite Note-1</li>
-        $(".references li").each(function (index) {
-            $(this).attr("id", "cite_note-" + (index + 1));
-            $(this).prepend("<a href=#cite_ref-" + (index + 1) + "><b>^</b></a> ");
-        });
-    }
-
-})(window);
+var references = function () {          
+    $("my-ref").each(function (index) {
+        $(this).attr("id", "cite_ref-" + (index + 1));
+        $(this).text("");
+        $(this).append("<a href=#cite_note-" + (index + 1) + "> [" + (index + 1) + "]</a>");
+    });
+    //  <li id="cite_note-1"><a href="#cite_ref-1"><b>^</b></a> Cite Note-1</li>
+    $(".references li").each(function (index) {
+        $(this).attr("id", "cite_note-" + (index + 1));
+        $(this).prepend("<a href=#cite_ref-" + (index + 1) + "><b>^</b></a> ");
+    });
+}
