44ab7d3edfd7b578fd5d27f1c77c77594b9813a0
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2016-08-02 20:39:26 +0200

    Update ref.js

--- js/ref.js
+++ js/ref.js
@@ -9,6 +9,7 @@
         //  <li id="cite_note-1"><a href="#cite_ref-1"><b>^</b></a> Cite Note-1</li>
         $(".references li").each(function (index) {
             $(this).attr("id", "cite_note-" + (index + 1));
+            $(this).attr("class", "text-decoration-none");
             $(this).prepend("<a href=#cite_ref-" + (index + 1) + "><b>^</b></a> ");
         });
     }
