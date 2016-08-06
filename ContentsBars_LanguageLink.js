ed5b088d71661713bd948136157a5e7a1b1f3ebf
Mauro Vezzoli <mauro.vezzoli@tiscali.it>
2016-08-06 21:07:42 +0200

    Update ContentsBars_LanguageLink.js

--- ContentsBars_LanguageLink.js
+++ ContentsBars_LanguageLink.js
@@ -157,6 +157,19 @@
                              else return;
                     };
                     
+        var references = function () {          
+                             $("my-ref").each(function (index) {
+                                 $(this).attr("id", "cite_ref-" + (index + 1));
+                                 $(this).text("");
+                                 $(this).append("<a href=#cite_note-" + (index + 1) + "> [" + (index + 1) + "]</a>");
+                             });
+                             //  <li id="cite_note-1"><a href="#cite_ref-1"><b>^</b></a> Cite Note-1</li>
+                             $(".references li").each(function (index) {
+                                 $(this).attr("id", "cite_note-" + (index + 1));
+                                 $(this).prepend("<a href=#cite_ref-" + (index + 1) + " class='text-decoration-none'><b>^</b></a> ");
+                             });
+       }
+                    
         var toUrlFormat =function (fragment) {
                return fragment.replace(/-\s/g,'').replace(/\s/g,'-').replace("'",'').toLowerCase();
         };
@@ -164,5 +177,6 @@
         window.searchBar=searchBar;
         window.myContentBar=myContentBar;
         window.languageLink=languageLink;
+        window.references=references;
     
 })(window);
